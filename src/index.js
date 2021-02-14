f91d844ad7b2ed2f8eb5795981eb1154b8830a08
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-14 15:27:53 -0600

    fixed my grammer in comments lol

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 /*
-everything in teh curly brackets
+everything in the curly brackets
 must be the same as in teh math file
 */
 import pi, { doublePi, triplePi } from "./math";
